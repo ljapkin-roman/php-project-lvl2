8f021f311eb2641ad55eba74e1ccd6c14287b318
roma <silvervaza@gmail.com>
2019-08-21 16:41:33 +0300

    add function filter

--- src/generateDiff.php
+++ src/generateDiff.php
@@ -1,7 +1,34 @@
 <?php
 namespace Ogurchik\generateDiff;
 
+function isFileExists($file)
+{
+    print_r($file . "\n");
+    if (!file_exists($file)) {
+        throw new \Exception("{$file} is not exists");
+    }
+}
+function getValidJson($str, $nameFile)
+{
+    $result = json_decode(file_get_contents($str), true);
+    if (json_last_error()) {
+        throw new \Exception("{$nameFile} is not valid json");
+    }
+    return $result;
+}
+function getRemovedItem($firstArr, $secondArr)
+{
+    
+}
 function generateDiff($firstFile, $secondFile)
 {
-    print_r("sup PHP!");
+    $pathToFirstFile ="./src/" . $firstFile;
+    $pathToSecondFile ="./src/" . $secondFile;
+    isFileExists($pathToFirstFile);
+    isFileExists($pathToSecondFile);
+    $data1 = getValidJson($pathToFirstFile, $firstFile);
+    $data2 = getValidJson($pathToSecondFile, $secondFile);
+    $sameItem = array_intersect_assoc($data1, $data2);
+    $ = array_diff($data1, $data2);
+    
 }
