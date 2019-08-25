d38af65826de8ce294092649d9f33953a41bfcd6
roma <silvervaza@gmail.com>
2019-08-25 18:51:18 +0300

    before pull

--- tests/AddTest.php
+++ tests/AddTest.php
@@ -7,7 +7,7 @@ class AddTest extends TestCase
     public function testAdd()
     {
         $stack = [];
-        $this->assertSame(9, add(3, 5));
+        $this->assertSame(8, add(3, 5));
         $this->assertSame(5, add(3, 2));
     }
 }
